9fb5fd493770baf6b9ce72b01f878e424d617c99
Volodymyr-2024 <itspec.ua@gmail.com>
2024-10-24 20:05:20 +0200

    Homework-10 React добавлена возможность хостинга!

--- React/Homework_10/vite.config.js
+++ React/Homework_10/vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Hausaufgabe/tree/main/React/Homework_10/dist"
-})
+  base: "/Hausaufgabe/tree/main/React/Homework_10/dist/",
+});
