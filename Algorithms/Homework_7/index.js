e68541a9a98f45c49aa3bdb7f18e2a03fb141e7c
Volodymyr-2024 <itspec.ua@gmail.com>
2025-02-18 20:48:34 +0100

    Added Homework_7 Algorithms 18-02-2025

---
+++ Algorithms/Homework_7/index.js
@@ -0,0 +1,14 @@
+function letterCounter(text) {
+  text = text.toLowerCase().replace(/[^a-zäöüß]/g, "");
+  let frequency = {};
+
+  for (let i = 0; i < text.length; i++) {
+    let char = text[i];
+    frequency[char] = (frequency[char] || 0) + 1;
+  }
+  return frequency;
+}
+
+let text =
+  "Dies ist ein Beispieltext für eine Häufigkeitsanalyse der Buchstaben.";
+console.log(letterCounter(text));
